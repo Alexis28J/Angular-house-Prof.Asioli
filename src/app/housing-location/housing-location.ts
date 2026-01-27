1f5786c0ee14f6af3d42265b7154571901cfe5c5
labstraction <labstraction@protonmail.com>
2026-01-27 10:26:49 +0100

    list

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { HousingLocationInfo } from '../model/housing-location-info';
 
 @Component({
   selector: 'app-housing-location',
   imports: [],
-  templateUrl: './housing-location.html',
+  template: `
+    <section class="listing">      
+      <img class="listing-photo" src="{{info().photo}}" alt="Exterior photo of {{ info().name }}" crossorigin/>      
+      <h2 class="listing-heading">{{ info().name }}</h2>      
+      <p class="listing-location">{{ info().city }}, {{ info().state }}</p>
+    </section>
+  `,
   styleUrl: './housing-location.scss',
 })
 export class HousingLocation {
+
+  info = input.required<HousingLocationInfo>();
 
 }
